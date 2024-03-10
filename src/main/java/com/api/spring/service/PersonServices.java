59a5557dc114495fb107d4a776d0d474f701fa3e
Renato Farias <santosrenato351@gmail.com>
2024-03-10 10:20:51 -0300

    adding hateoas

--- src/main/java/com/api/spring/service/PersonServices.java
+++ src/main/java/com/api/spring/service/PersonServices.java
@@ -4,7 +4,7 @@ import com.api.spring.Exceptions.ResourceNotFoundException;
 import com.api.spring.controller.PersonController;
 import com.api.spring.model.Person;
 import com.api.spring.repository.PersonRepository;
-import com.api.spring.mapper.ModelMapperConvert;
+import com.api.spring.mapper.ModelMapperConverter;
 import org.springframework.stereotype.Service;
 import com.api.spring.vo.PersonVO;
 
@@ -14,15 +14,15 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
-public class PersonService implements Serializable {
+public class PersonServices implements Serializable {
     private final PersonRepository personRepository;
 
-    public PersonService(PersonRepository personRepository) {
+    public PersonServices(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
     public List<PersonVO> findAll() {
-        var persons = ModelMapperConvert.parseListObjects(personRepository.findAll(), PersonVO.class);
+        var persons = ModelMapperConverter.parseListObjects(personRepository.findAll(), PersonVO.class);
         persons.forEach(person -> person.add(linkTo(methodOn(PersonController.class)
                 .findById(person.getKey())).withSelfRel()));
         return persons;
@@ -31,14 +31,14 @@ public class PersonService implements Serializable {
     public PersonVO findById(Long id) {
         var entity = personRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Person ID not found."));
-        var vo = ModelMapperConvert.parseObject(entity, PersonVO.class);
+        var vo = ModelMapperConverter.parseObject(entity, PersonVO.class);
         vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
         return vo;
     }
 
     public PersonVO create(PersonVO personVO) {
-        var entity = ModelMapperConvert.parseObject(personVO, Person.class);
-        var vo = ModelMapperConvert.parseObject(personRepository.save(entity), PersonVO.class);
+        var entity = ModelMapperConverter.parseObject(personVO, Person.class);
+        var vo = ModelMapperConverter.parseObject(personRepository.save(entity), PersonVO.class);
         vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
         return vo;
     }
@@ -52,7 +52,7 @@ public class PersonService implements Serializable {
         entity.setAddress(person.getAddress());
         entity.setGender(person.getGender());
 
-        var vo = ModelMapperConvert.parseObject(personRepository.save(entity), PersonVO.class);
+        var vo = ModelMapperConverter.parseObject(personRepository.save(entity), PersonVO.class);
         vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
         return vo;
     }
