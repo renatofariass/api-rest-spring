4f623804be9abd7ebc26f0e4652f77b682001978
Renato Farias <santosrenato351@gmail.com>
2024-03-10 10:19:14 -0300

    updating class names

--- src/main/java/com/api/spring/controller/PersonController.java
+++ src/main/java/com/api/spring/controller/PersonController.java
@@ -1,6 +1,6 @@
 package com.api.spring.controller;
 
-import com.api.spring.service.PersonService;
+import com.api.spring.service.PersonServices;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,42 +14,42 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "api/person/v1")
 public class PersonController implements Serializable {
-    private PersonService personService;
+    private PersonServices personServices;
 
-    public PersonController(PersonService personService) {
-        this.personService = personService;
+    public PersonController(PersonServices personServices) {
+        this.personServices = personServices;
     }
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<List<PersonVO>> findAll() {
-        List<PersonVO> personList = personService.findAll();
+        List<PersonVO> personList = personServices.findAll();
         return ResponseEntity.ok().body(personList);
     }
 
     @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<PersonVO> findById(@PathVariable Long id) {
-        PersonVO person = personService.findById(id);
+        PersonVO person = personServices.findById(id);
         return ResponseEntity.ok().body(person);
     }
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
                  produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<PersonVO> create(@RequestBody PersonVO obj) {
-        PersonVO person = personService.create(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(person.getId()).toUri();
+        PersonVO person = personServices.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(person.getKey()).toUri();
         return ResponseEntity.created(uri).body(person);
     }
 
     @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
                 produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<PersonVO> update(@RequestBody PersonVO obj) {
-        PersonVO person = personService.update(obj);
+        PersonVO person = personServices.update(obj);
         return ResponseEntity.ok().body(person);
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        personService.delete(id);
+        personServices.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
