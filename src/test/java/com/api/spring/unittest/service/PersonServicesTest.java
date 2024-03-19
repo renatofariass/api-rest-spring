40002e4d181e15caaf1854f4006d6a73977cb7a7
Renato Farias <santosrenato351@gmail.com>
2024-03-19 19:55:22 -0300

    service test cases

---
+++ src/test/java/com/api/spring/unittest/service/PersonServicesTest.java
@@ -0,0 +1,172 @@
+package com.api.spring.unittest.service;
+
+import com.api.spring.exceptions.RequiredObjectIsNullException;
+import com.api.spring.model.Person;
+import com.api.spring.repository.PersonRepository;
+import com.api.spring.service.PersonServices;
+import com.api.spring.unittest.mapper.mock.MockPerson;
+import com.api.spring.vo.PersonVO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class PersonServicesTest {
+    @InjectMocks
+    private PersonServices service;
+    @Mock
+    private PersonRepository repository;
+    private MockPerson input;
+
+
+    @BeforeEach
+    void setUpMocks() {
+        MockitoAnnotations.openMocks(this);
+        input = new MockPerson();
+    }
+
+    @Test
+    void findById() {
+        Person person = input.mockEntity(1);
+        person.setId(1L);
+        when(repository.findById(1L)).thenReturn(Optional.of(person));
+
+        var result = service.findById(1L);
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+
+        assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Address Test1", result.getAddress());
+        assertEquals("Female", result.getGender());
+    }
+
+
+    @Test
+    void findAll() {
+        List<Person> list = input.mockEntityList();
+        when(repository.findAll()).thenReturn(list);
+
+        var people = service.findAll();
+        assertNotNull(people);
+        assertEquals(14, people.size());
+
+        var personOne = people.get(1);
+        assertNotNull(personOne);
+        assertNotNull(personOne.getKey());
+        assertNotNull(personOne.getLinks());
+
+        assertTrue(personOne.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+        assertEquals("First Name Test1", personOne.getFirstName());
+        assertEquals("Last Name Test1", personOne.getLastName());
+        assertEquals("Address Test1", personOne.getAddress());
+        assertEquals("Female", personOne.getGender());
+
+        var personSeven = people.get(7);
+        assertNotNull(personSeven);
+        assertNotNull(personSeven.getKey());
+        assertNotNull(personSeven.getLinks());
+
+        assertTrue(personSeven.toString().contains("links: [</api/person/v1/7>;rel=\"self\"]"));
+        assertEquals("First Name Test7", personSeven.getFirstName());
+        assertEquals("Last Name Test7", personSeven.getLastName());
+        assertEquals("Address Test7", personSeven.getAddress());
+        assertEquals("Female", personSeven.getGender());
+
+        var personTwelve = people.get(12);
+        assertNotNull(personTwelve);
+        assertNotNull(personTwelve.getKey());
+        assertNotNull(personTwelve.getLinks());
+
+        assertTrue(personTwelve.toString().contains("links: [</api/person/v1/12>;rel=\"self\"]"));
+        assertEquals("First Name Test12", personTwelve.getFirstName());
+        assertEquals("Last Name Test12", personTwelve.getLastName());
+        assertEquals("Address Test12", personTwelve.getAddress());
+        assertEquals("Male", personTwelve.getGender());
+    }
+
+    @Test
+    void create() {
+        Person entity = input.mockEntity(1);
+
+        Person persisted = entity;
+        persisted.setId(1L);
+
+        PersonVO vo = input.mockVO(1);
+        vo.setKey(1L);
+
+        when(repository.save(entity)).thenReturn(persisted);
+
+        var result = service.create(vo);
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+
+        assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Address Test1", result.getAddress());
+        assertEquals("Female", result.getGender());
+    }
+
+    @Test
+    void testCreateWithNullPerson() {
+        Exception exception = assertThrows(RequiredObjectIsNullException.class, () -> {
+            service.create(null);
+        });
+
+        assertEquals("It is not allowed to persist a null object!", exception.getMessage());
+    }
+
+
+    @Test
+    void update() {
+        Person entity = input.mockEntity(1);
+
+        Person persisted = entity;
+        persisted.setId(1L);
+
+        PersonVO vo = input.mockVO(1);
+        vo.setKey(1L);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(entity));
+        when(repository.save(entity)).thenReturn(persisted);
+
+        var result = service.update(vo);
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+
+        assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Address Test1", result.getAddress());
+        assertEquals("Female", result.getGender());
+    }
+
+    @Test
+    void testUpdateWithNullPerson() {
+        Exception exception = assertThrows(RequiredObjectIsNullException.class, () -> {
+            service.update(null);
+        });
+
+        assertEquals("It is not allowed to persist a null object!", exception.getMessage());
+    }
+
+    @Test
+    void delete() {
+        Person person = input.mockEntity(1);
+        person.setId(1L);
+        when(repository.findById(1L)).thenReturn(Optional.of(person));
+        service.delete(1L);
+    }
+}
