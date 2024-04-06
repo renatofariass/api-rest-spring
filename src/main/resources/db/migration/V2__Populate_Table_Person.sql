5a9cb9fa14a3e702104a57b9cb7f885e85a1a38f
Renato Farias <santosrenato351@gmail.com>
2024-04-05 22:02:30 -0300

    removing id column (unnecessary)

--- src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -1,8 +1,8 @@
-INSERT INTO person (id, address, first_name, gender, last_name) VALUES
-       (1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
-       (2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
-       (3, 'Porbandar - India', 'Indira', 'Female', 'Gandhi'),
-       (4, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
-       (5, 'Kentucky - US', 'Muhammad', 'Male', 'Ali'),
-       (6, 'Mvezo – South Africa', 'Nelson', 'Male', 'Mvezo'),
-       (7, 'Smiljan - Croácia', 'Nikola', 'Male', 'Tesla');
+INSERT INTO person (address, first_name, gender, last_name) VALUES
+       ('São Paulo', 'Ayrton', 'Male', 'Senna'),
+       ('Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+       ('Porbandar - India', 'Indira', 'Female', 'Gandhi'),
+       ('Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+       ('Kentucky - US', 'Muhammad', 'Male', 'Ali'),
+       ('Mvezo – South Africa', 'Nelson', 'Male', 'Mvezo'),
+       ('Smiljan - Croácia', 'Nikola', 'Male', 'Tesla');
