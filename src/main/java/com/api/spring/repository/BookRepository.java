d6fb07322e6e66a0cd926469547191761cddc0c8
Renato Farias <santosrenato351@gmail.com>
2024-04-05 21:40:11 -0300

    creating book repository

---
+++ src/main/java/com/api/spring/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.api.spring.repository;
+
+import com.api.spring.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
