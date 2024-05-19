68ab5693f7708d8d2ec85b8e88d82d4856f4c99f
Renato Farias <santosrenato351@gmail.com>
2024-05-18 22:03:56 -0300

    implementing jwt token provider

---
+++ src/main/java/com/api/spring/securityJwt/JwtTokenProvider.java
@@ -0,0 +1,48 @@
+package com.api.spring.securityJwt;
+
+import com.api.spring.vo.TokenVO;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class JwtTokenProvider {
+    @Value("${security.jwt.token.secret-key:secret}")
+    private String secretKey = "secret";
+
+    @Value("${security.jwt.token.expire-lenght:3600000}")
+    private long validityInMilliseconds = 3600000; //1h
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    Algorithm algorithm = null;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        algorithm = Algorithm.HMAC256(secretKey.getBytes());
+    }
+
+    public TokenVO createAccessToken(String username, List<String> roles) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+        var accessToken = getAcessToken(username, roles, now, validity);
+        var refreshToken = getRefreshToken(username, roles, now);
+        return new TokenVO(username, true, now, validity, accessToken, refreshToken);
+    }
+
+    private String getAcessToken(String username, List<String> roles, Date now, Date validity) {
+        return null;
+    }
+
+    private String getRefreshToken(String username, List<String> roles, Date now) {
+    }
+}
