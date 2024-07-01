f37a6cea4f27228a465fca13650d77fb5d902026
Renato Farias <santosrenato351@gmail.com>
2024-07-01 11:26:17 -0300

    updating users

--- src/main/resources/db/migration/V8__Populate_Table_Users.sql
+++ src/main/resources/db/migration/V8__Populate_Table_Users.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (user_name, full_name, password, account_non_expired, account_non_locked, credentials_non_expired,
                    enabled)
-VALUES ('Renato', 'Renato Farias', '19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f',
+VALUES ('renato', 'Renato Farias', '$2a$12$80O8DNOHSf4zxTu.cmzLUe7G09x.1/nSl7KzaSkOgweyr49lBg4Z2',
         TRUE, TRUE, TRUE, TRUE),
-       ('flavio', 'Flavio Costa', '75ec349c1b0ef4ee7b249d0b83ae4861853f3aa77bce8c4b15f28cd43c6424ab4f29df431831bb0d',
+       ('flavio', 'Flavio Costa', '$2a$12$80O8DNOHSf4zxTu.cmzLUe7G09x.1/nSl7KzaSkOgweyr49lBg4Z2',
         TRUE, TRUE, TRUE, TRUE);
