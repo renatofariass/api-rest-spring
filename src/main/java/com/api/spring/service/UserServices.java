9318c9fdf40bef3bd1a326d5e2db7ec00299c347
Renato Farias <santosrenato351@gmail.com>
2024-07-01 11:13:20 -0300

    excluding unused imports

--- src/main/java/com/api/spring/service/UserServices.java
+++ src/main/java/com/api/spring/service/UserServices.java
@@ -1,23 +1,10 @@
 package com.api.spring.service;
 
-import com.api.spring.controller.PersonController;
-import com.api.spring.exceptions.RequiredObjectIsNullException;
-import com.api.spring.exceptions.ResourceNotFoundException;
-import com.api.spring.mapper.ModelMapperConverter;
-import com.api.spring.model.Person;
-import com.api.spring.repository.PersonRepository;
 import com.api.spring.repository.UserRepository;
-import com.api.spring.vo.PersonVO;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.io.Serializable;
-import java.util.List;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 public class UserServices implements UserDetailsService {
