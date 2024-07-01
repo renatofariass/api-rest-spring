12709dee1668e1b861e2f6663255c27b4128eba9
Renato Farias <santosrenato351@gmail.com>
2024-07-01 11:08:12 -0300

    fixing UserRepository query

--- src/main/java/com/api/spring/repository/UserRepository.java
+++ src/main/java/com/api/spring/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.api.spring.repository;
 
-import com.api.spring.model.Book;
 import com.api.spring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT User AS u FROM User WHERE u.userName =:userName")
+    @Query("SELECT u FROM User u WHERE u.userName =:userName")
     User findByUsername(@Param("userName") String userName);
 }
