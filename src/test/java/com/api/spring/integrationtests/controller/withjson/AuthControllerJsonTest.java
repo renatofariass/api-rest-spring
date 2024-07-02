2b5e9fbc31f060f1a2649f192e3e9257a256ae6e
Renato Farias <santosrenato351@gmail.com>
2024-07-02 12:19:12 -0300

    creating login controller integration tests with json

---
+++ src/test/java/com/api/spring/integrationtests/controller/withjson/AuthControllerJsonTest.java
@@ -0,0 +1,69 @@
+package com.api.spring.integrationtests.controller.withjson;
+
+import com.api.spring.configs.TestConfigs;
+import com.api.spring.integrationtests.testcontainers.AbstractIntegrationTest;
+import com.api.spring.integrationtests.vo.AccountCredentialsVO;
+import com.api.spring.integrationtests.vo.TokenVO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class AuthControllerJsonTest extends AbstractIntegrationTest {
+    private static TokenVO tokenVO;
+
+    @Test
+    @Order(1)
+    public void testSignin() throws JsonMappingException, JsonProcessingException {
+        AccountCredentialsVO user = new AccountCredentialsVO("renato", "senha");
+
+        tokenVO = given()
+                .basePath("auth/signin")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(user)
+                .when()
+                    .post()
+                .then()
+                    .statusCode(200)
+                .extract()
+                    .body()
+                        .as(TokenVO.class);
+
+        assertNotNull(tokenVO.getAccessToken());
+        assertNotNull(tokenVO.getRefreshToken());
+    }
+
+    @Test
+    @Order(2)
+    public void testRefreshToken() throws JsonMappingException, JsonProcessingException {
+        AccountCredentialsVO user = new AccountCredentialsVO("renato", "senha");
+
+       var newTokenVO = given()
+                .basePath("auth/refresh")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .pathParam("username", tokenVO.getUsername())
+                .header(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + tokenVO.getRefreshToken())
+                .when()
+                    .put("{username}")
+                .then()
+                    .statusCode(200)
+                .extract()
+                    .body()
+                        .as(TokenVO.class);
+
+        assertNotNull(newTokenVO.getAccessToken());
+        assertNotNull(newTokenVO.getRefreshToken());
+    }
+}
+
+
